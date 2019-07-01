a6d6446e1cdfa801c77ab454bb31db7746405cfa
Rodrigo Garcês <rmgarces1@gmail.com>
2019-07-01 10:18:58 -0300

    Corrigido erro no script de instalação

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ cd ~/target_generator
 catkin build
 cp src/a1slam.sh ~/a1slam.sh
 cp src/a2slam.sh ~/a2slam.sh
-cp src/default.rviz ~/$USER/.rviz/default.rviz
+cp src/default.rviz ~/.rviz/default.rviz
