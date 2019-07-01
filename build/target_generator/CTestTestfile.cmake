e3111a2e385cd152374902e1545fb4a4fc6f3db3
Rodrigo Garcês <rmgarces1@gmail.com>
2019-07-01 09:51:37 -0300

    Removida pasta conflitante build

--- build/target_generator/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raphael/catkin_ws/src/target_generator
-# Build directory: /home/raphael/catkin_ws/build/target_generator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
