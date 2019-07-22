b2548bfda8f6d0604bcefa81f938a437a0a6a305
Rodrigo Garcês <rmgarces1@gmail.com>
2019-07-22 09:56:51 -0300

    Adicionado script de instalação completa do ROS

---
+++ install_ros.sh
@@ -0,0 +1,35 @@
+#Made by Rodrigo Garcês
+#	2019
+
+#add ROS repository
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+#add ROS key
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+#update repository to include ROS information
+sudo apt-get update
+
+#install ROS full-desktop
+sudo apt-get install ros-kinetic-desktop-full
+
+#initialize rosdep
+sudo rosdep init
+rosdep update
+
+#load ROS enviroment variables automatically
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+#install ROS dependencies
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
+
+#download V-REP
+wget http://coppeliarobotics.com/files/V-REP_PRO_EDU_V3_6_2_Ubuntu16_04.tar.xz -O vrep.tar.xz
+
+#install vrep on current user
+tar -xf vrep.tar.xz 
+
+#download project dependencies
+sudo apt-get install ros-kinetic-navigation ros-kinetic-move-base ros-kinetic-gmapping
+
--- install_workspace.sh
+++ install_workspace.sh
@@ -1,5 +1,9 @@
+#Made by Rodrigo Garcês
+#	2019
+
 cd ~/target_generator
 catkin build
 cp src/a1slam.sh ~/a1slam.sh
 cp src/a2slam.sh ~/a2slam.sh
+mkdir -p ~/.rviz
 cp src/default.rviz ~/.rviz/default.rviz
