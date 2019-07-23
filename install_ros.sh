12d3a84cfc6d26a81f1ef41071ee3e6e4fbe0d27
Rodrigo Garcês <rmgarces1@gmail.com>
2019-07-23 16:07:13 -0300

    Corrigidos pequenos bugs no instalador do ROS

--- install_ros.sh
+++ install_ros.sh
@@ -8,10 +8,10 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 #update repository to include ROS information
-sudo apt-get update
+sudo apt update
 
 #install ROS full-desktop
-sudo apt-get install ros-kinetic-desktop-full
+sudo apt install -y ros-kinetic-desktop-full
 
 #initialize rosdep
 sudo rosdep init
@@ -22,14 +22,17 @@ echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 #install ROS dependencies
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
+sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
 
 #download V-REP
+cd ~
 wget http://coppeliarobotics.com/files/V-REP_PRO_EDU_V3_6_2_Ubuntu16_04.tar.xz -O vrep.tar.xz
 
 #install vrep on current user
-tar -xf vrep.tar.xz 
+tar -xf vrep.tar.xz
 
 #download project dependencies
-sudo apt-get install ros-kinetic-navigation ros-kinetic-move-base ros-kinetic-gmapping
+sudo apt install -y ros-kinetic-navigation ros-kinetic-move-base ros-kinetic-gmapping
 
+#reload bashrc file to load ROS extensions
+source ~/.bashrc
