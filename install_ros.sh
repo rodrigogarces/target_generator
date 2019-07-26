20c70a5b9fe23d93b35d9c359f67fc550ff6f48f
Rodrigo Garcês <rmgarces1@gmail.com>
2019-07-26 10:24:59 -0300

    Adicionado atalho do V-REP no menu(launcher) (Ainda sem ícone)

--- install_ros.sh
+++ install_ros.sh
@@ -32,6 +32,17 @@ sudo wget http://coppeliarobotics.com/files/V-REP_PRO_EDU_V3_6_2_Ubuntu16_04.tar
 sudo tar -xf vrep.tar.xz
 sudo rm vrep.tar.xz
 
+#create vrep launcher on menu (currently without icon, but is merely aesthetic)
+echo -e '[Desktop Entry]
+Name=V-REP
+Comment=Launch V-REP
+Comment[pt_BR]=Inicia o V-REP
+Exec=/usr/local/V-REP_PRO_EDU_V3_6_2_Ubuntu16_04/vrep.sh
+#Icon=/opt/lampp/htdocs/favicon.ico
+Terminal=false
+Type=Application
+Categories=Network;' | sudo tee /usr/share/applications/vrep.desktop
+
 #download project dependencies
 sudo apt install -y ros-kinetic-navigation ros-kinetic-move-base ros-kinetic-gmapping
 
